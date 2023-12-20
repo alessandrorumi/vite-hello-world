b328d2495d7ccb8c6500d2c8cc3025703fe05d05
alessandrorumi <rumi.alessandro21@gmail.com>
2023-12-20 14:57:06 +0100

    Bonus (Jumbotron in nuovo component)

---
+++ src/components/appHero.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  name: 'AppHero'
+}
+</script>
+
+<template>
+  <section>
+    <div class="jumbo">
+      <h1>Vite + Vue</h1>
+      <img src="../assets/jumbo.webp" alt="">
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.jumbo {
+  position: relative;
+}
+
+h1 {
+  position: absolute;
+  top: 1rem;
+  left: 50%;
+  transform: translateX(-50%);
+}
+
+img {
+  width: 100%;
+}
+</style>
